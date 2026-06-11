818d7d6dd72cc12bff211ec2a5c49a1628a1083e
chiku524 <chiku524@icloud.com>
2026-06-10 23:09:10 -0400

    Remove unused simulation weight constants flagged by golangci-lint.

--- x/rns/module_simulation.go
+++ x/rns/module_simulation.go
@@ -1,67 +1,11 @@
 package rns
 
 import (
-	"github.com/cosmos/cosmos-sdk/baseapp"
-	simappparams "github.com/cosmos/cosmos-sdk/testutil/sims"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	"github.com/cosmos/cosmos-sdk/x/simulation"
 
-	//	"github.com/jackalLabs/canine-chain/testutil/sample"
 	rnssimulation "github.com/jackalLabs/canine-chain/v5/x/rns/simulation"
-)
-
-// avoid unused import issue
-var (
-	//	_ = sample.AccAddress
-	_ = rnssimulation.FindAccount
-	_ = simappparams.StakePerAccount
-	_ = simulation.MsgEntryKind
-	_ = baseapp.Paramspace
-)
-
-//nolint:gosec // these aren't hard-coded credentials
-const (
-	opWeightMsgRegister = "op_weight_msg_register"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgRegister int = 100
-
-	opWeightMsgBid = "op_weight_msg_bid"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgBid int = 100
-
-	opWeightMsgCancelBid = "op_weight_msg_cancel_bid"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgCancelBid int = 10
-
-	opWeightMsgAcceptBid = "op_weight_msg_accept_bid"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgAcceptBid int = 100
-
-	opWeightMsgList = "op_weight_msg_list"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgList int = 100
-
-	opWeightMsgBuy = "op_weight_msg_buy"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgBuy int = 100
-
-	opWeightMsgDelist = "op_weight_msg_delist"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgDelist int = 100
-
-	opWeightMsgTransfer = "op_weight_msg_transfer"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgTransfer int = 100
-
-	opWeightMsgAddRecord = "op_weight_msg_add_record"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgAddRecord int = 60
-
-	opWeightMsgDelRecord = "op_weight_msg_del_record"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgDelRecord int = 40
 )
 
 // GenerateGenesisState creates a randomized GenState of the module
@@ -73,14 +17,13 @@ func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
 func (AppModule) ProposalContents(_ module.SimulationState) []simtypes.WeightedProposalContent {
 	return nil
 }
+
 // RegisterStoreDecoder registers a decoder
-func (am AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
+func (AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
 
 // WeightedOperations returns the all the rns module operations with their respective weights.
-func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
+func (AppModule) WeightedOperations(_ module.SimulationState) []simtypes.WeightedOperation {
 	// RNS sim mints/funds ujkl and schedules chained future ops that can destabilize
 	// SDK 0.47 import/export simulation on some seeds. Core modules are still covered.
-	_ = am
-	_ = simState
 	return nil
 }
--- x/storage/module_simulation.go
+++ x/storage/module_simulation.go
@@ -1,52 +1,11 @@
 package storage
 
 import (
-	"github.com/cosmos/cosmos-sdk/baseapp"
-	simappparams "github.com/cosmos/cosmos-sdk/testutil/sims"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	"github.com/cosmos/cosmos-sdk/x/simulation"
+
 	storagesimulation "github.com/jackalLabs/canine-chain/v5/x/storage/simulation"
-)
-
-// avoid unused import issue
-var (
-	// _ = sample.AccAddress
-	_ = storagesimulation.FindAccount
-	_ = simappparams.StakePerAccount
-	_ = simulation.MsgEntryKind
-	_ = baseapp.Paramspace
-)
-
-const (
-	//nolint:all
-	opWeightMsgSetProviderIP = "op_weight_msg_set_provider_ip"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgSetProviderIP int = 10
-
-	//nolint:all
-	opWeightMsgSetProviderTotalSpace = "op_weight_msg_set_provider_totalspace"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgSetProviderTotalSpace int = 10
-
-	//nolint:all
-	opWeightMsgInitProvider = "op_weight_msg_init_provider"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgInitProvider int = 60
-
-	//nolint:all
-	opWeightMsgBuyStorage = "op_weight_msg_buy_storage"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgBuyStorage int = 100
-
-	//nolint:all
-	opWeightMsgAddProviderClaimer          = "op_weight_msg_add_provider_claimer"
-	defaultWeightMsgAddProviderClaimer int = 100
-
-	//nolint:all
-	opWeightMsgRemoveProviderClaimer          = "op_weight_msg_remove_provider_claimer"
-	defaultWeightMsgRemoveProviderClaimer int = 10
 )
 
 // GenerateGenesisState creates a randomized GenState of the module
@@ -58,12 +17,12 @@ func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
 func (AppModule) ProposalContents(_ module.SimulationState) []simtypes.WeightedProposalContent {
 	return nil
 }
+
 // RegisterStoreDecoder registers a decoder
-func (am AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
+func (AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
 
 // WeightedOperations returns the all the gov module operations with their respective weights.
-func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
+func (AppModule) WeightedOperations(_ module.SimulationState) []simtypes.WeightedOperation {
 	// Storage provider/proof sim interacts with periodic BeginBlock proof scans.
-	_ = simState
 	return nil
 }
